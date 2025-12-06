dfe393579886da03a12038abf720348ff9bd9011
Your Name <you@example.com>
2025-12-06 04:25:31 -0800

    first commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,20 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import User from "../model/user.js";
 
 export const protect = async (req, res, next) => {
-        let token;
-        if (
-            req.headers.authorization.startsWith("Bearer") 
-        ) {
-            try {
-                token = req.headers.authorization.split(" ")[1];
-                const decoded = jwt.verify(token, process.env.SECRET_KEY); 
-                req.user = await User.findById(decoded.id).select('-password');
-                next();
-            } catch (error) {
-                return res.status(401).json({ message: "Not authorized, invalid token" });
-            }           
+    let token;
+     
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: 'Not authorized, token failed' });
         }
-        if (!token) {
-            return res.status(401).json({ message: "Not authorized, no token" });
-        }
-    };
+    }
+    if (!token) {
+        return res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
--- route/user.js
+++ route/user.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { createStudents, deleteUser, getAllStudents, getUserById, loginUser, updateUser } from '../controller/user.js';
-import { protect } from '../middleware/AuthMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 router.post('/register', createStudents)
-router.get('/', protect,  getAllStudents)
+router.get('/', protect, getAllStudents)
 router.get('/:id', getUserById)
 router.post('/login', loginUser)
 router.put('/update/:id', updateUser )
